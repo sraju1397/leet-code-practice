bf0d2ed9294964fa4cbb610b1cc8262e1174ab95
Raju Samala <sraju1397@live.com>
2024-11-06 23:31:19 +0530

    2. merge sorted lists

---
+++ 2_mege_2_sorted_lists.js
@@ -0,0 +1,26 @@
+// 21. Merge Two Sorted Lists
+var mergeLists = function (list1, list2) {
+  var dummy = new ListNode(0);
+  var current = dummy;
+  while (list1.next != null && list2.next != null) {
+    if (list1.value <= lise2.value) {
+      current.next = list1;
+      list1 = list1.next;
+    } else {
+      current.next = list2;
+      llist2 = list2.next;
+    }
+    current = current.next;
+  }
+
+  if (list1.next !== null)
+    current.next = list1;
+
+  if (list2.next !== null)
+    current.next = list2;
+
+
+
+  return dummy.next;
+
+}
