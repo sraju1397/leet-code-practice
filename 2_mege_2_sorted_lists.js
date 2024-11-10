b863b857bc61c65e1f02de5cb142e033d566f4c2
Raju Samala <sraju1397@live.com>
2024-11-10 17:45:02 +0530

    spell error correction

--- 2_mege_2_sorted_lists.js
+++ 2_mege_2_sorted_lists.js
@@ -3,12 +3,12 @@ var mergeLists = function (list1, list2) {
   var dummy = new ListNode(0);
   var current = dummy;
   while (list1.next != null && list2.next != null) {
-    if (list1.value <= lise2.value) {
+    if (list1.value <= list2.value) {
       current.next = list1;
       list1 = list1.next;
     } else {
       current.next = list2;
-      llist2 = list2.next;
+      list2 = list2.next;
     }
     current = current.next;
   }
